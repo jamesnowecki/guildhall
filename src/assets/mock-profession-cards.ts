82a2cb120ce60c29bf823c9b26c73c1a89c7b4ca
James Nowecki <jamesnowecki@gmail.com>
2020-06-17 22:49:32 +0100

    started the looping process functions to instantiate a profession deck

---
+++ src/assets/mock-profession-cards.ts
@@ -0,0 +1,19 @@
+import { ProfessionCard, AssassinCard, TraderCard, FarmerCard, DancerCard, WeaverCard, HistorianCard } from "../app/templates/profession-card";
+
+const createSubDeck = (profession: ProfessionCard) => {
+
+    let professionCardSubDeck: ProfessionCard[] = [];
+
+    //needs to instantiate 4 cards of each color for the profession and push them into the array
+    return professionCardSubDeck;
+}
+
+
+
+
+const createProfessionDeck = () => {
+
+    let professionCardDeck: ProfessionCard[] = [];
+
+
+}
--- src/assets/mock-vpcards.ts
+++ src/assets/mock-vpcards.ts
@@ -1,7 +1,5 @@
 import { VPCard, VPCard2, VPCard3A, VPCard3B, VPCard4A, VPCard4B, VPCard4C, VPCard5, VPCard7, VPCard9 } from "../app/templates/vp-card";
 
-
-
 const createVPDeck = () => {
 
     let vpCardDeck: VPCard[] = [];
